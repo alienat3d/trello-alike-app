15dbe95841ac2a0740e243d3d8d07033c49079d9
Al Zaplin <zaplin.dev@gmail.com>
2025-06-20 22:12:27 +0300

    add(code & notes) For #11: Vue Draggable Sortable classes: styling the tasks dragging process

--- components/TrelloBoard.vue
+++ components/TrelloBoard.vue
@@ -64,8 +64,11 @@ const isAltPressed = useKeyModifier("Alt");
             <!-- 6.2 Также, как мы и делали с колонками, поместим их в слот, а данные извлечём из переменной "task" и укажем тип "Task". -->
             <!-- 6.3 Мы также можем избавиться от "v-for" & "key". -->
             <!-- 6.4 Осталось добавит "handle", чтобы перетаскивать задачи можно было лишь по зажатию кнопки мышки в специальной зоне задачи. И также добавим соотв. комп. в [components\TrelloBoardTask.vue]. -->
+            <!-- 8.2 Однако, чтобы применить стили нам нужно добавить элементу задач доп. div-обёртку. -->
             <template #item="{ element: task }: { element: Task }">
-              <TrelloBoardTask :task="task" />
+              <div>
+                <TrelloBoardTask :task="task" />
+              </div>
             </template>
           </draggable>
           <footer><button class="text-gray-500">+ Add a Card</button></footer>
@@ -74,3 +77,24 @@ const isAltPressed = useKeyModifier("Alt");
     </draggable>
   </div>
 </template>
+
+<!-- * 8.0 В оригинальном «Trello» при перетаскивании элементы задач наклоняются в одну сторону, мы можем сделать тоже самое. «SoratableJS» даёт нам три класса для разных состояний: "sortable-chosen" (когда мы только начали перетаскивать элемент кликнув на нём и удерживая кнопку мыши.), "sortable-drag" (когда мы начали движение удерживая элемент) и "sortable-ghost" (применяется к дублирующему полупрозрачному отображению элемента, показывающее где он упадёт, если отпустить кнопку мыши). В нашем примере "sortable-chosen" не пригодится. -->
+<!-- 8.1 При перетаскивании элемента мы хотим слегка наклонять вбок элемент задачи наподобие как в оригинальном «Trello». ↑ -->
+<!-- 8.3 Теперь займёмся стилизацией "призрака", для достижения эффекта, как у оригинального «Trello» лучше всего подойдёт псевдоэлемент. -->
+<style>
+@import "tailwindcss";
+
+/* .sortable-chosen {} */
+
+.sortable-drag .task {
+  transform: rotate(4deg);
+}
+
+.sortable-ghost .task {
+  @apply relative;
+}
+.sortable-ghost .task::after {
+  content: "";
+  @apply absolute top-0 right-0 bottom-0 left-0 rounded bg-slate-300;
+}
+</style>
